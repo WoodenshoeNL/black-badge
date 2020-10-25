c0a966e69f11ae220f72de778fbd4a35f0066f2b
Michel Klomp <michel.klomp@sentia.com>
2020-10-25 14:04:20 +0100

    add generate ssh key

--- install/htb/prep.sh
+++ install/htb/prep.sh
@@ -15,3 +15,10 @@ mkdir reverse-shell
 cd reverse-shell
 cp ~/my_data/g/black-badge/Doc/CC/Reverse-Shell-scripts/php-reverse-shell.php ./prss.php
 
+
+#generate keypair for target logon
+cd ~/htb
+mkdir rsa
+cd rsa
+ssh-keygen -t rsa -b 4096 -C "htb@woodenshoe" -f ./htb_rsa -q -N ""
+
