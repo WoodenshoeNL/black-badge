beaff4108621e0f3946244e86a0f201b5d86bf54
Michel <michel@woodenshoe.org>
2021-03-14 16:32:11 +0100

    change elf run

--- htb/challenges/ch-reg-elf.py
+++ htb/challenges/ch-reg-elf.py
@@ -4,9 +4,10 @@ from pwn import *
 #context.log_level = 'DEBUG'
 context(os='linux', arch='amd64')
 
-elf = ELF('/home/kali/Downloads/reg')
+elf = context.binary = ELF('/home/kali/Downloads/reg')
 
-info("symbols = %#x", hex(elf.symbols[b'run']))
+#info("symbols = %#x", hex(elf.symbols[b'run']))
 
-info("plt = %#x", hex(elf.plt[b'run']))
+#info("plt = %#x", hex(elf.plt[b'run']))
 
+info("%#x target", elf.symbols.run)
