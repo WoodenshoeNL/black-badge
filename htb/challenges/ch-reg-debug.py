96a12997297e5bb73ca12245705e55c2649ebcf0
Michel <michel@woodenshoe.org>
2021-03-14 15:41:10 +0100

    add reg debug script

---
+++ htb/challenges/ch-reg-debug.py
@@ -0,0 +1,26 @@
+
+from pwn import *
+
+context.log_level = 'DEBUG'
+context(os='linux', arch='amd64')
+context.terminal = ["tmux", "splitw", "-h"]
+
+padding_length = 104
+ret_offset = -96
+
+log.info('Create IO')
+#io = remote('', )
+io = process('/home/kali/Downloads/reg')
+pid = gdb.attach(io, '''
+set follow-fork-mode child
+break *(main)
+continue
+''')
+
+
+payload = cyclic(200)
+
+log.info('Send Payload')
+io.sendlineafter('name : ', payload)
+
+io.interactive()
