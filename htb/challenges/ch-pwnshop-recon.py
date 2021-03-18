ce4529e293a5ee4210c4d19eaef37757ee7f9035
Michel <michel@woodenshoe.org>
2021-03-18 22:39:37 +0100

    change after line

--- htb/challenges/ch-pwnshop-recon.py
+++ htb/challenges/ch-pwnshop-recon.py
@@ -27,9 +27,9 @@ io.sendlineafter('>', '2')
 payload = cyclic(32)
 
 log.info('Send Payload')
-io.sendlineafter('? ', payload)
-io.sendlineafter('? ', payload)
-io.sendlineafter('? ', payload)
+io.sendlineafter(' ', payload)
+io.sendlineafter(' ', payload)
+io.sendlineafter(' ', payload)
 
 io.wait()
 
