9089761ad0f3d81e30be03634c6e5b3b3484da92
Michel <michel@woodenshoe.org>
2021-06-23 22:08:15 +0200

    add print alias

--- install/oscp/script/create-alias.sh
+++ install/oscp/script/create-alias.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+alias qprint="cat ~/black-badge/install/oscp/script/create-alias.sh"
 alias qn="~/black-badge/install/oscp/recon/nmap.sh"
 alias qf="~/black-badge/install/oscp/recon/ffuf.sh"
