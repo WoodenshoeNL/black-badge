f25c91cc80e362de6881080271d4e08a1dfeef65
Michel <michel@woodenshoe.org>
2021-06-23 22:16:38 +0200

    add echo recon path

--- install/oscp/script/create-alias.sh
+++ install/oscp/script/create-alias.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 alias qprint="cat ~/black-badge/install/oscp/script/create-alias.sh"
+alias qpath="echo ~/black-badge/install/oscp/recon/"
 alias qn="~/black-badge/install/oscp/recon/nmap.sh"
 alias qf="~/black-badge/install/oscp/recon/ffuf.sh"
