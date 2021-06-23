9aab429cd0cbf55a62f4c681613fa87e8a48a668
Michel <michel@woodenshoe.org>
2021-06-23 20:12:21 +0200

    change output format

--- install/oscp/recon/nmap.sh
+++ install/oscp/recon/nmap.sh
@@ -2,6 +2,6 @@
 
 echo $PWD
 
-sudo nmap -Pn -sV -p- -aO nmap-full-quick-$1.txt $1
+sudo nmap -Pn -sV -p- -oA nmap-full-quick-$1.txt $1
 
 #sudo nmap -Pn -sV -sS -A -p- --script vuln -O -oA nmap-full-$1.txt $1
