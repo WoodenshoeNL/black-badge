b1a722bab2d7248e1935e290f6a3ace1191f5ed7
Michel <michel@woodenshoe.org>
2021-06-24 16:08:37 +0200

    add tmux info and log

--- install/oscp/script/create-alias.sh
+++ install/oscp/script/create-alias.sh
@@ -2,5 +2,7 @@
 
 alias qprint="cat ~/black-badge/install/oscp/script/create-alias.sh"
 alias qpath="echo ~/black-badge/install/oscp/recon/"
+alias qml="~/black-badge/install/oscp/script/move-tmuxLogs.sh"
+alias qtmux="~/black-badge/install/oscp/script/print-tmuxinfo.sh"
 alias qn="~/black-badge/install/oscp/recon/nmap.sh"
 alias qf="~/black-badge/install/oscp/recon/ffuf.sh"
---
+++ install/oscp/script/move-tmuxLogs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mv ~/lab/logging/* $PWD
