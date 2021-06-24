ddefa3c2465b02f106f6a47296d544af718ea3ff
Michel <michel@woodenshoe.org>
2021-06-24 10:45:06 +0200

    update nmap script

--- install/oscp/recon/nmap.sh
+++ install/oscp/recon/nmap.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-echo "Running: sudo nmap -Pn -sV -p- -T4 -oN nmap-full-quick.txt $1"
-sudo nmap -Pn -sV -p- -T4 -oN nmap-full-quick.txt $1
+echo "################################################################"
+echo "# Start Nmap scans for $1"
+echo "################################################################"
+
+echo "Running: sudo nmap -Pn -sV --top-ports 20 -T4 -oN nmap-top20-quick.txt $1"
+sudo nmap -Pn -sV --top-ports 20 -T4 -oN nmap-top20-quick.txt $1
 
 echo "################################################################"
 
-echo "sudo nmap -Pn -sV -sS -A -p- --script vuln -T4 -O -oN nmap-full-A.txt $1"
-sudo nmap -Pn -sV -sS -A -p- --script vuln -T4 -O -oN nmap-full-A.txt $1
+echo "Running: sudo nmap -Pn -sS -p- -T4 -oN nmap-full-quick.txt $1"
+sudo nmap -Pn -sS -p- -T4 -oN nmap-full-quick.txt $1
+
+echo "################################################################"
+
+echo "sudo nmap -Pn -sV -sS -A -p- --script vuln,discovery --version-all -T4 -O -oN nmap-full-A.txt $1"
+sudo nmap -Pn -sV -sS -A -p- --script vuln,discovery --version-all -T4 -O -oN nmap-full-A.txt $1
+
+echo "################################################################"
+
+echo "sudo nmap -Pn -sV -sU -A -p- --script vuln,discovery --version-all -T4 -O -oN nmap-full-UDP-A.txt $1"
+sudo nmap -Pn -sV -sS -A -p- --script vuln,discovery --version-all -T4 -O -oN nmap-full-UDP-A.txt $1
