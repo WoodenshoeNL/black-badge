e08db5ca7d9af775554fb5fc2059990d6599b05d
Michel <michel@woodenshoe.org>
2021-06-24 17:09:38 +0200

    change create alias

--- install/oscp/script/create-alias.sh
+++ install/oscp/script/create-alias.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-alias qprint=cat ~/black-badge/install/oscp/script/create-alias.sh
-alias qpath=echo ~/black-badge/install/oscp/recon/
-alias qml=~/black-badge/install/oscp/script/move-tmuxLogs.sh
-alias qtmux=~/black-badge/install/oscp/script/print-tmuxinfo.sh
-alias qn=~/black-badge/install/oscp/recon/nmap.sh
-alias qf=~/black-badge/install/oscp/recon/ffuf.sh
+alias qprint="cat ~/black-badge/install/oscp/script/create-alias.sh"
+alias qpath="echo '~/black-badge/install/oscp/recon/'"
+alias qml="~/black-badge/install/oscp/script/move-tmuxLogs.sh"
+alias qtmux="~/black-badge/install/oscp/script/print-tmuxinfo.sh"
+alias qn="~/black-badge/install/oscp/recon/nmap.sh"
+alias qf="~/black-badge/install/oscp/recon/ffuf.sh"
