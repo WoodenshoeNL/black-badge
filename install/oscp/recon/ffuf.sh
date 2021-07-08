98c9be94c5955af1cdaf1cfc4f7286978314bf45
Michel <michel@woodenshoe.org>
2021-07-08 14:01:39 +0200

    add ffuf ext and recursion

--- install/oscp/recon/ffuf.sh
+++ install/oscp/recon/ffuf.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-ffuf -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt -u http://$1/FUZZ > ffuf-init-$1.txt
+ffuf -w /usr/share/dirb/wordlists/big.txt -u http://$1/FUZZ > ffuf-init-$1.txt
+
+ffuf -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt -u http://$1/FUZZ >> ffuf-init-$1.txt
+
+
+ffuf -w /usr/share/dirb/wordlists/big.txt -e .sh,.txt,.php,.htb,.py,.pl,.bak,.htm,.html,.js,.asp,.aspx,.json,.yml -u http://$1/FUZZ > ffuf-ext-$1.txt
+
+ffuf -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt -e .sh,.txt,.php,.htb,.py,.pl,.bak,.htm,.html,.js,.asp,.aspx,.json,.yml -u http://$1/FUZZ >> ffuf-ext-$1.txt
+
+
+ffuf -w /usr/share/dirb/wordlists/big.txt -recursion -recursion-depth 3 -u http://$1/FUZZ > ffuf-recursion-$1.txt
+
+ffuf -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt -recursion -recursion-depth 3 -u http://$1/FUZZ >> ffuf-recursion-$1.txt
