fde305d6cfef14ec89c2267186825a0b5f7db4e4
Michel <michel@woodenshoe.org>
2021-07-15 10:37:28 +0200

    remove add-slash with -x ferox

--- install/oscp/recon/feroxbuster.sh
+++ install/oscp/recon/feroxbuster.sh
@@ -2,11 +2,11 @@
 
 feroxbuster --url http://$1 -o ferox-init-$1.txt
 
-feroxbuster -w /usr/share/dirb/wordlists/big.txt --url http://$1 -o ferox-init-b-$1.txt
-feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url http://$1 -o ferox-init-m-$1.txt
+#feroxbuster -w /usr/share/dirb/wordlists/big.txt --url http://$1 -o ferox-init-b-$1.txt
+#feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url http://$1 -o ferox-init-m-$1.txt
 
 
-feroxbuster --url http://$1 -o ferox-extended-$1.txt --add-slash --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
+feroxbuster --url http://$1 -o ferox-extended-$1.txt --add-slash --extract-links
 
-feroxbuster -w /usr/share/dirb/wordlists/big.txt --url http://$1 -o ferox-extended-b-$1.txt --add-slash --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
-feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url http://$1 -o ferox-extended-m-$1.txt --add-slash --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
+#feroxbuster -w /usr/share/dirb/wordlists/big.txt --url http://$1 -o ferox-extended-b-$1.txt --add-slash --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
+feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url http://$1 -o ferox-extended-m-$1.txt --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
