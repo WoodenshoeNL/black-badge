b56643e045065ca28168b5b923594ed1227cf37f
Michel <michel@woodenshoe.org>
2021-07-15 10:59:30 +0200

    add --dont-filter wildcard

--- install/oscp/recon/feroxssl.sh
+++ install/oscp/recon/feroxssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-feroxbuster --url https://$1 -o ferox-ssl-init-$1.txt --insecure
+feroxbuster --url https://$1 -o ferox-ssl-init-$1.txt --insecure --dont-filter
 
-feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url https://$1 -o ferox-ssl-extended-m-$1.txt --insecure --add-slash --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
+feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url https://$1 -o ferox-ssl-extended-m-$1.txt --insecure --dont-filter --add-slash --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
