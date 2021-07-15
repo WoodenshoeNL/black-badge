8f2b8193e68353b88ba14e029f4ba2420405806b
Michel <michel@woodenshoe.org>
2021-07-15 11:02:39 +0200

    move --add-slash

--- install/oscp/recon/feroxssl.sh
+++ install/oscp/recon/feroxssl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-feroxbuster --url https://$1 -o ferox-ssl-init-$1.txt --insecure --dont-filter --filter-status 403
+feroxbuster --url https://$1 -o ferox-ssl-init-$1.txt --insecure --dont-filter --filter-status 403 --add-slash
 
-feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url https://$1 -o ferox-ssl-extended-m-$1.txt --filter-status 403 --insecure --dont-filter --add-slash --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
+feroxbuster -w /usr/share/dirbuster/wordlists/directory-list-lowercase-2.3-medium.txt --url https://$1 -o ferox-ssl-extended-m-$1.txt --filter-status 403 --insecure --dont-filter --extract-links -x php htm html json js xml asp aspx txt bak sh yml py pl
