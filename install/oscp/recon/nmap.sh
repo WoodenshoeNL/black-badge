618098c64f61bf48523dc80cdeff2b2ecf1f0841
Michel <michel@woodenshoe.org>
2021-09-15 22:29:55 +0200

    simplyfy nmap script

--- install/oscp/recon/nmap.sh
+++ install/oscp/recon/nmap.sh
@@ -5,24 +5,9 @@ echo "# Start Nmap scans for $1"
 echo "################################################################"
 
 echo "Running: sudo nmap -Pn -sV --top-ports 20 -T4 -oN nmap-top20-quick.txt $1"
-sudo nmap -Pn -sV --top-ports 20 --min-rate 10000 -T4 -oN nmap-top20-quick.txt $1
+sudo nmap -Pn -sS --top-ports 20 --min-rate 10000 -T4 -oN nmap-top20-quick.txt $1
 
 echo "################################################################"
 
 echo "Running: sudo nmap -Pn -sS -p- -T4 -oN nmap-full-quick.txt $1"
-sudo nmap -Pn -sS -p- --min-rate 10000 -T4 -oN nmap-full-quick.txt $1
-
-echo "################################################################"
-
-echo "sudo nmap -Pn -sS -p- --script vuln --min-rate 10000 -T4 -O -oN nmap-full-vuln.txt $1"
-sudo nmap -Pn -sS -p- --script vuln --min-rate 10000 -T4 -oN nmap-full-vuln.txt $1
-
-echo "################################################################"
-
-echo "sudo nmap -Pn -sV -sS -A -p- --script discovery --version-all --min-rate 10000 -T4 -O -oN nmap-full-Discover.txt $1"
-sudo nmap -Pn -sV -sS -A -p- --script discovery --version-all --min-rate 10000 -T4 -O -oN nmap-full-Discover.txt $1
-
-echo "################################################################"
-
-echo "sudo nmap -Pn -sU -p- --min-rate 10000 -T4 -oN nmap-full-UDP.txt $1"
-sudo nmap -Pn -sU -p- --min-rate 10000 -T4 -oN nmap-full-UDP.txt $1
+sudo nmap -Pn -sS -sV -p- --min-rate 10000 -T4 -oN nmap-full-quick.txt $1
